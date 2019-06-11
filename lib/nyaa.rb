e863b80075fc2eab096c668ebb979aa4388c4332
Anthony Poon <marquis.andras@gmail.com>
2019-06-12 01:59:02 +1000

    Initial commit

---
+++ lib/nyaa.rb
@@ -0,0 +1,68 @@
+require "open-uri"
+require "uri"
+require "nokogiri"
+
+class Nyaa
+
+  module Filter
+    NoFilter = "0"
+    NoRemakes = "1"
+    TrustedOnly = "2"
+  end
+
+  module Category
+    AllCategories="0_0"
+
+    module Anime
+      MusicVideo = "1_1"
+      EnglishTranslated = "1_2"
+      NonEnglishTranslated = "1_3"
+      Raw = "1_4"
+    end
+  end
+
+  def initialize(args={})
+    params = defaults.merge(args)
+
+    @filter = params[:filter]
+    @category = params[:category]
+    @query = params[:query]
+    @page = params[:page]
+
+    @base = "https://nyaa.si"
+  end
+
+  def defaults
+    { :filter => Nyaa::Filter::NoFilter,
+      :category => Nyaa::Category::AllCategories,
+      :query => "",
+      :page => 1 }
+  end
+
+  def html
+    uri.read
+  end
+
+  def uri
+    URI.join @base, "?#{querystring}"
+  end
+
+  def querystring
+    URI.encode_www_form(
+      "f" => @filter,
+      "c" => @category,
+      "q" => @query,
+      "p" => @page
+    )
+  end
+
+  def torrents
+    links
+      .map { |link| link["href"] } \
+      .select { |href| href =~ /\.torrent$/ }
+  end
+
+  def links
+    Nokogiri::HTML(html).css(".torrent-list tr a")
+  end
+end
