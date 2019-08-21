3fe0bd29adcb44ed3960dda61e626d9e37f47890
Anthony Poon <marquis.andras@gmail.com>
2019-08-21 11:49:41 +0000

    Add HorribleSubs searcher

---
+++ lib/horrible_subs.rb
@@ -0,0 +1,16 @@
+require "nyaa_search"
+require "nyaa_scraper"
+
+class HorribleSubs
+  def self.search_for(series)
+    search_criteria = NyaaSearch.new(
+      :filter => Nyaa::Filter::TrustedOnly,
+      :category => Nyaa::Category::Anime::EnglishTranslated,
+      :query => "HorribleSubs 1080p -batch #{series}",
+      :page => 1
+    )
+
+    scraper = NyaaScraper.new(search_criteria)
+    return scraper.magnets
+  end
+end
---
+++ test/test_horrible_subs.rb
@@ -0,0 +1,10 @@
+require "minitest"
+require "horrible_subs"
+
+class TestHorribleSubs < MiniTest::Test
+  def test_that_anime_from_horrible_subs_can_be_found
+    found = HorribleSubs.search_for("one punch man s2")
+    num_episodes = 12
+    assert_equal num_episodes, found.length
+  end
+end
